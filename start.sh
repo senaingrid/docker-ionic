2ed251d09396450f54b2476dedf111f4ac417af0
Juergen <juergen.schackmann@googlemail.com>
2017-09-22 13:39:13 +0200

    fixed start script to allways start with a install

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 
 # TODO: only do this for certain commands (serve, test?), otherwise only do a regular install
 
+npm install &
+
 # Watch package.json and re-install on changes
 node -e "
 const fs = require('fs');
@@ -20,16 +22,5 @@ fs.watchFile('package.json',() => {
   });
 });
 console.log('A watcher on package.json has been setup and will trigger a install on file changes\n')
-
-exec('${PACKAGE_MANAGER} install',(error, stdout, stderr) => {
-    if (stdout) console.log(stdout);
-    if (stderr) console.error(stderr);
-    if (error) {
-      console.error('${PACKAGE_MANAGER} install has not finished sucessfully: '+ error);
-    } else {
-      console.log('${PACKAGE_MANAGER} install has finished succesfully.\n')
-    }
-});" &
-
-
+" &
 exec "$@"
