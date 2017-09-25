b71338faf203027dbaaca3c89f67f031705bc504
Unknown <juergen.schackmann@googlemail.com>
2017-09-25 10:24:05 +0200

    Update start.sh to only install with CLI parameter

--- start.sh
+++ start.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 # set -e
 
-# TODO: only do this for certain commands (serve, test?), otherwise only do a regular install
+declare -a ARGS
+for var in "$@"; do
+    # Ignore known bad arguments
+    if [ "$var" = '--install' ] || [ "$var" = '-i' ]; then
+        npm install;
+        continue
+    fi
+    ARGS[${#ARGS[@]}]="$var"
+done
 
-npm install;
-
-# Watch package.json and re-install on changes
-# node -e "
-# const fs = require('fs');
-# exec = require('child_process').exec;
-# fs.watchFile('package.json',() => {
-#   console.log('package.json has changed; a install is triggered.\n');
-#   exec('${PACKAGE_MANAGER} install',(error, stdout, stderr) => {
-#       if (stdout) console.log(stdout);
-#       if (stderr) console.error(stderr);
-#       if (error) {
-#         console.error('${PACKAGE_MANAGER} install has not finished sucessfully: '+ error);
-#       } else {
-#         console.log('${PACKAGE_MANAGER} install has finished succesfully.\n')
-#       }
-#   });
-# });
-# console.log('A watcher on package.json has been setup and will trigger a install on file changes\n')
-# " &
-exec "$@"
+exec "${ARGS[@]}"
